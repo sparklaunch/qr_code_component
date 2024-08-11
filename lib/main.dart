d61785626faaf0caf66489263f60751cbe99ce4c
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-11 11:53:28 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_component/screens/main_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: MainScreen(),
+      ),
+    ),
+  );
+}
